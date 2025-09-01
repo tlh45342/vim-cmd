38c61569f6bfe756a9cfe0fc87351aa05a7b577c
Thomas Hamilton <88941733+tlh45342@users.noreply.github.com>
2025-08-31 21:03:45 -0400

    Update vim-cmd.c

--- src/vim-cmd.c
+++ src/vim-cmd.c
@@ -12,6 +12,13 @@
 #include <stdbool.h>
 
 #ifdef _WIN32
+  #define strcasecmp  _stricmp
+  #define strncasecmp _strnicmp
+#else
+  #include <strings.h>   // for strcasecmp on macOS/Linux/BSD
+#endif
+
+#ifdef _WIN32
   #define WIN32_LEAN_AND_MEAN
   #include <winsock2.h>
   #include <ws2tcpip.h>
